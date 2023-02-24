78f93faf6c44b664cb1b5045420cf013090ca10d
parksuyeon <clapyyyyy@gmail.com>
2023-02-24 15:55:51 +0900

    수업내용

---
+++ aaa.js
@@ -0,0 +1,9 @@
+let pokemon = ["피카츄", "라이츄","파이리"];
+
+console.log(pokemon[0]);
+
+for(let i = 0; i <pokemon.length; i++){
+  console.log(pokemon[i]);
+}
+
+// 터미널창에 저장된 폴더 확인하고 node aaa.js
---
+++ aaa.py
@@ -0,0 +1,26 @@
+rubin = 1
+print(rubin)
+rubin = "바보"
+print(rubin)
+
+kdt = {
+  "삼백오호" : "홀쭉",
+  "공욱재" : "미남",
+}
+print(kdt["공욱재"])
+
+kdt_order = ["우리는","개발자","입니다"]
+print(kdt_order[0], kdt_order[1], kdt_order[2])
+for index in kdt_order:
+  print(index)
+
+
+  def percent_calc(real_value, total_value): #def정의하다
+    result = (real_value / total_value) *100
+    return result
+
+
+
+#파이썬은 간결하다
+
+# 터미널창에 저장된 폴더 확인하고 python aaa.py
